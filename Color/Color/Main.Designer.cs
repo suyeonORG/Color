ff03929a430f2606a4be762beeecc4830dc9e938
suyeonORG <contact@suyeon.org>
2018-05-16 00:52:13 +0200

    [INIT]

---
+++ Color/Color/Main.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace Color
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.UltraRedFrame = new System.Windows.Forms.PictureBox();
+            this.PinkFrame = new System.Windows.Forms.PictureBox();
+            this.RedFrame = new System.Windows.Forms.PictureBox();
+            this.BlueFrame = new System.Windows.Forms.PictureBox();
+            this.SepiaFrame = new System.Windows.Forms.PictureBox();
+            this.ResetFrame = new System.Windows.Forms.PictureBox();
+            this.BNFrame = new System.Windows.Forms.PictureBox();
+            this.ContrastFrame = new System.Windows.Forms.PictureBox();
+            this.MainFrame = new System.Windows.Forms.PictureBox();
+            this.VintageFrame = new System.Windows.Forms.PictureBox();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.UltraRedFrame)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PinkFrame)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RedFrame)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BlueFrame)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SepiaFrame)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ResetFrame)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BNFrame)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ContrastFrame)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MainFrame)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.VintageFrame)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // UltraRedFrame
+            // 
+            this.UltraRedFrame.BackColor = System.Drawing.Color.Transparent;
+            this.UltraRedFrame.Location = new System.Drawing.Point(11, 397);
+            this.UltraRedFrame.Name = "UltraRedFrame";
+            this.UltraRedFrame.Size = new System.Drawing.Size(128, 72);
+            this.UltraRedFrame.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.UltraRedFrame.TabIndex = 27;
+            this.UltraRedFrame.TabStop = false;
+            this.UltraRedFrame.Click += new System.EventHandler(this.UltraRedFrame_Click);
+            this.UltraRedFrame.MouseLeave += new System.EventHandler(this.UltraRedFrame_MouseLeave);
+            this.UltraRedFrame.MouseMove += new System.Windows.Forms.MouseEventHandler(this.UltraRedFrame_MouseMove);
+            // 
+            // PinkFrame
+            // 
+            this.PinkFrame.BackColor = System.Drawing.Color.Transparent;
+            this.PinkFrame.Location = new System.Drawing.Point(144, 397);
+            this.PinkFrame.Name = "PinkFrame";
+            this.PinkFrame.Size = new System.Drawing.Size(128, 72);
+            this.PinkFrame.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.PinkFrame.TabIndex = 26;
+            this.PinkFrame.TabStop = false;
+            this.PinkFrame.Click += new System.EventHandler(this.PinkFrame_Click);
+            this.PinkFrame.MouseLeave += new System.EventHandler(this.PinkFrame_MouseLeave);
+            this.PinkFrame.MouseMove += new System.Windows.Forms.MouseEventHandler(this.PinkFrame_MouseMove);
+            // 
+            // RedFrame
+            // 
+            this.RedFrame.BackColor = System.Drawing.Color.Transparent;
+            this.RedFrame.Location = new System.Drawing.Point(145, 321);
+            this.RedFrame.Name = "RedFrame";
+            this.RedFrame.Size = new System.Drawing.Size(128, 72);
+            this.RedFrame.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.RedFrame.TabIndex = 25;
+            this.RedFrame.TabStop = false;
+            this.RedFrame.Click += new System.EventHandler(this.RedFrame_Click);
+            this.RedFrame.MouseLeave += new System.EventHandler(this.RedFrame_MouseLeave);
+            this.RedFrame.MouseMove += new System.Windows.Forms.MouseEventHandler(this.RedFrame_MouseMove);
+            // 
+            // BlueFrame
+            // 
+            this.BlueFrame.BackColor = System.Drawing.Color.Transparent;
+            this.BlueFrame.Location = new System.Drawing.Point(12, 321);
+            this.BlueFrame.Name = "BlueFrame";
+            this.BlueFrame.Size = new System.Drawing.Size(128, 72);
+            this.BlueFrame.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.BlueFrame.TabIndex = 24;
+            this.BlueFrame.TabStop = false;
+            this.BlueFrame.Click += new System.EventHandler(this.BlueFrame_Click);
+            this.BlueFrame.MouseLeave += new System.EventHandler(this.BlueFrame_MouseLeave);
+            this.BlueFrame.MouseMove += new System.Windows.Forms.MouseEventHandler(this.BlueFrame_MouseMove);
+            // 
+            // SepiaFrame
+            // 
+            this.SepiaFrame.BackColor = System.Drawing.Color.Transparent;
+            this.SepiaFrame.Location = new System.Drawing.Point(278, 321);
+            this.SepiaFrame.Name = "SepiaFrame";
+            this.SepiaFrame.Size = new System.Drawing.Size(128, 72);
+            this.SepiaFrame.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.SepiaFrame.TabIndex = 23;
+            this.SepiaFrame.TabStop = false;
+            this.SepiaFrame.Click += new System.EventHandler(this.SepiaFrame_Click);
+            this.SepiaFrame.MouseLeave += new System.EventHandler(this.SepiaFrame_MouseLeave);
+            this.SepiaFrame.MouseMove += new System.Windows.Forms.MouseEventHandler(this.SepiaFrame_MouseMove);
+            // 
+            // ResetFrame
+            // 
+            this.ResetFrame.BackColor = System.Drawing.Color.Transparent;
+            this.ResetFrame.Location = new System.Drawing.Point(12, 245);
+            this.ResetFrame.Name = "ResetFrame";
+            this.ResetFrame.Size = new System.Drawing.Size(128, 72);
+            this.ResetFrame.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.ResetFrame.TabIndex = 22;
+            this.ResetFrame.TabStop = false;
+            this.ResetFrame.Click += new System.EventHandler(this.ResetFrame_Click);
+            this.ResetFrame.MouseLeave += new System.EventHandler(this.ResetFrame_MouseLeave);
+            this.ResetFrame.MouseMove += new System.Windows.Forms.MouseEventHandler(this.ResetFrame_MouseMove);
+            // 
+            // BNFrame
+            // 
+            this.BNFrame.BackColor = System.Drawing.Color.Transparent;
+            this.BNFrame.Location = new System.Drawing.Point(278, 245);
+            this.BNFrame.Name = "BNFrame";
+            this.BNFrame.Size = new System.Drawing.Size(128, 72);
+            this.BNFrame.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.BNFrame.TabIndex = 21;
+            this.BNFrame.TabStop = false;
+            this.BNFrame.Click += new System.EventHandler(this.BNFrame_Click);
+            this.BNFrame.MouseLeave += new System.EventHandler(this.BNFrame_MouseLeave);
+            this.BNFrame.MouseMove += new System.Windows.Forms.MouseEventHandler(this.BNFrame_MouseMove);
+            // 
+            // ContrastFrame
+            // 
+            this.ContrastFrame.BackColor = System.Drawing.Color.Transparent;
+            this.ContrastFrame.Location = new System.Drawing.Point(145, 245);
+            this.ContrastFrame.Name = "ContrastFrame";
+            this.ContrastFrame.Size = new System.Drawing.Size(128, 72);
+            this.ContrastFrame.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.ContrastFrame.TabIndex = 20;
+            this.ContrastFrame.TabStop = false;
+            this.ContrastFrame.Click += new System.EventHandler(this.ContrastFrame_Click);
+            this.ContrastFrame.MouseLeave += new System.EventHandler(this.ContrastFrame_MouseLeave);
+            this.ContrastFrame.MouseMove += new System.Windows.Forms.MouseEventHandler(this.ContrastFrame_MouseMove);
+            // 
+            // MainFrame
+            // 
+            this.MainFrame.BackColor = System.Drawing.Color.Transparent;
+            this.MainFrame.Location = new System.Drawing.Point(12, 11);
+            this.MainFrame.Name = "MainFrame";
+            this.MainFrame.Size = new System.Drawing.Size(395, 221);
+            this.MainFrame.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.MainFrame.TabIndex = 19;
+            this.MainFrame.TabStop = false;
+            this.MainFrame.Click += new System.EventHandler(this.MainFrame_Click);
+            this.MainFrame.MouseLeave += new System.EventHandler(this.MainFrame_MouseLeave);
+            this.MainFrame.MouseMove += new System.Windows.Forms.MouseEventHandler(this.MainFrame_MouseMove);
+            // 
+            // VintageFrame
+            // 
+            this.VintageFrame.BackColor = System.Drawing.Color.Transparent;
+            this.VintageFrame.Location = new System.Drawing.Point(278, 397);
+            this.VintageFrame.Name = "VintageFrame";
+            this.VintageFrame.Size = new System.Drawing.Size(128, 72);
+            this.VintageFrame.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.VintageFrame.TabIndex = 28;
+            this.VintageFrame.TabStop = false;
+            this.VintageFrame.Click += new System.EventHandler(this.VintageFrame_Click);
+            this.VintageFrame.MouseLeave += new System.EventHandler(this.VintageFrame_MouseLeave);
+            this.VintageFrame.MouseMove += new System.Windows.Forms.MouseEventHandler(this.VintageFrame_MouseMove);
+            // 
+            // saveFileDialog
+            // 
+            this.saveFileDialog.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog_FileOk);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(419, 480);
+            this.Controls.Add(this.VintageFrame);
+            this.Controls.Add(this.UltraRedFrame);
+            this.Controls.Add(this.PinkFrame);
+            this.Controls.Add(this.RedFrame);
+            this.Controls.Add(this.BlueFrame);
+            this.Controls.Add(this.SepiaFrame);
+            this.Controls.Add(this.ResetFrame);
+            this.Controls.Add(this.BNFrame);
+            this.Controls.Add(this.ContrastFrame);
+            this.Controls.Add(this.MainFrame);
+            this.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.White;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Suyeon - Color Studio";
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Main_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Main_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Main_MouseUp);
+            ((System.ComponentModel.ISupportInitialize)(this.UltraRedFrame)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PinkFrame)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RedFrame)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BlueFrame)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SepiaFrame)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ResetFrame)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BNFrame)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ContrastFrame)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MainFrame)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.VintageFrame)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.PictureBox UltraRedFrame;
+        internal System.Windows.Forms.PictureBox PinkFrame;
+        internal System.Windows.Forms.PictureBox RedFrame;
+        internal System.Windows.Forms.PictureBox BlueFrame;
+        internal System.Windows.Forms.PictureBox SepiaFrame;
+        internal System.Windows.Forms.PictureBox ResetFrame;
+        internal System.Windows.Forms.PictureBox BNFrame;
+        internal System.Windows.Forms.PictureBox ContrastFrame;
+        internal System.Windows.Forms.PictureBox MainFrame;
+        internal System.Windows.Forms.PictureBox VintageFrame;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+    }
+}
+
---
+++ Color/Color/Main.cs
@@ -0,0 +1,340 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Color
+{
+    /* Author: Suyeon */
+    public partial class Main : Form
+    {
+        static Graphics g;
+        static Bitmap source;
+
+        /* Filters */
+        float[][] sepia = { new float[] { 0.393f, 0.349f, 0.272f, 0, 0 }, new float[] { 0.769f, 0.686f, 0.534f, 0, 0 }, new float[] { 0.189f, 0.168f, 0.131f, 0, 0 }, new float[] { 0, 0, 0, 0.5f, 0 }, new float[] { 0, 0, 0, 0, 1 } };
+        float[][] blue = { new float[] { 0.393f, 0.349f, 0.272f, 0, 0 }, new float[] { 0.769f, 0.686f, 0.534f, 0, 0 }, new float[] { 0.4f, 0.4f, 0.4f, 0, 0 }, new float[] { 0, 0, 0, 0.5f, 0 }, new float[] { 0, 0, 120, 0, 1 } };
+        float[][] red = { new float[] { 0.393f, 0.349f, 0.272f, 0, 0 }, new float[] { 0.769f, 0.686f, 0.534f, 0, 0 }, new float[] { 0.7f, 0.7f, 0.7f, 0, 0 }, new float[] { 0, 0, 0, 0.5f, 0 }, new float[] { 120, 0, 0, 0, 1 } };
+        float[][] ultrared = { new float[] { 0.25f, 0.25f, 0.25f, 0, 0 }, new float[] { 0.2f, 0.2f, 0.2f, 0, 0 }, new float[] { 0.7f, 0.7f, 1.2f, 0, 0 }, new float[] { 0, 0, 0, 0.7f, 0 }, new float[] { 250, 0, 0, 0, 1 } };
+        float[][] pink = { new float[] { 0.393f, 0.349f, 0.272f, 0, 0 }, new float[] { 0.769f, 0.686f, 0.534f, 0, 0 }, new float[] { 0.7f, 0.7f, 0.7f, 0, 0 }, new float[] { 0, 0, 0, 0.5f, 0 }, new float[] { 250, 0, 250, 0, 1 } };
+        float[][] vintage  = { new float[] { 0.4f, 0.4f, 0.4f, 0, 0 }, new float[] { 0.4f, 0.4f, 0.4f, 0, 0 }, new float[] { 0.4f, 0.4f, 0.4f, 0, 0 }, new float[] { 0, 0, 0, 0.7f, 0 }, new float[] { 0, 0, 0, 0, 1 } };
+
+        /* Form Drag */
+        int x;
+        int y;
+        Boolean drag;
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            source = (Bitmap)Bitmap.FromFile("Template.min.jpg");
+            ResetFrame.Image = source;
+            MainFrame.Image = source;
+
+            GrayScale(BNFrame);
+            ColorFilter(blue, BlueFrame);
+            ColorFilter(sepia, SepiaFrame);
+            ColorFilter(red, RedFrame);
+            ColorFilter(ultrared, UltraRedFrame);
+            ColorFilter(pink, PinkFrame);
+            Contrast(ContrastFrame);
+            Vintage(vintage, VintageFrame);     
+        }
+
+        void GrayScale(PictureBox control)
+        {
+            this.Text = "Color - Processing...";
+            this.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+
+            if (control.Image == null)
+            {
+                control.Image = new Bitmap(source);
+            }
+
+            Bitmap local_source = new Bitmap(control.Image);
+
+            int x = 0;
+            int y = 0;
+            int color = 0;
+
+            for (x = 0; x <= local_source.Width - 1; x++)
+            {
+                for (y = 0; y <= local_source.Height - 1; y++)
+                {
+                    color = (local_source.GetPixel(x, y).R + local_source.GetPixel(x, y).G + local_source.GetPixel(x, y).B) / 3;
+                    local_source.SetPixel(x, y, System.Drawing.Color.FromArgb(color, color, color));
+                }
+            }
+            control.Image = local_source;
+
+            this.Text = "Color - Ready !";
+            this.Cursor = System.Windows.Forms.Cursors.Arrow;
+        }
+
+        private void ColorFilter(float[][] values, PictureBox control)
+        {
+            this.Text = "Color - Processing...";
+            this.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+
+            if (control.Image == null)
+            {
+                control.Image = new Bitmap(source);
+            }
+
+            this.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+            Bitmap local_source = new Bitmap(control.Image);
+            System.Drawing.Imaging.ColorMatrix filterMatrix = new System.Drawing.Imaging.ColorMatrix(values);
+            System.Drawing.Imaging.ImageAttributes IA = new System.Drawing.Imaging.ImageAttributes();
+            IA.SetColorMatrix(filterMatrix);
+            Bitmap filter = new Bitmap(local_source);
+            using (Graphics G = Graphics.FromImage(filter))
+            {
+                G.DrawImage(local_source, new Rectangle(0, 0, filter.Width, filter.Height), 0, 0, filter.Width, filter.Height, GraphicsUnit.Pixel, IA);
+            }
+
+            control.Image = filter;
+
+            this.Text = "Color - Ready !";
+            this.Cursor = System.Windows.Forms.Cursors.Arrow;
+        }
+
+        public void Contrast(PictureBox control)
+        {
+            float _contrastFactor = 0.04F * 28;
+            control.Image = new Bitmap(source);
+            Graphics g = Graphics.FromImage(control.Image);
+            ImageAttributes IA = new ImageAttributes();
+            ColorMatrix filterMatrix = new ColorMatrix(new float[][] { new float[] { _contrastFactor, 0f, 0f, 0f, 0f }, new float[] { 0f, _contrastFactor, 0f, 0f, 0f }, new float[] { 0f, 0f, _contrastFactor, 0f, 0f }, new float[] { 0f, 0f, 0f, 1f, 0f }, new float[] { 0.001f, 0.001f, 0.001f, 0f, 1f } });
+            IA.SetColorMatrix(filterMatrix);
+            g.DrawImage(control.Image, new Rectangle(0, 0, control.Image.Width, control.Image.Height), 0, 0, control.Image.Width, control.Image.Height, GraphicsUnit.Pixel, IA);
+            g.Dispose();
+            IA.Dispose();
+        }
+
+        public void Vintage(float[][] values, PictureBox control)
+        {
+            this.Text = "Color - Processing...";
+            this.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+
+            if (control.Image == null)
+            {
+                control.Image = new Bitmap(source);
+            }
+
+            System.Drawing.Imaging.ColorMatrix filterMatrix = new System.Drawing.Imaging.ColorMatrix(values);
+            System.Drawing.Imaging.ImageAttributes IA = new System.Drawing.Imaging.ImageAttributes();
+            IA.SetColorMatrix(filterMatrix);
+            Bitmap filter = new Bitmap(control.Image);
+            using (Graphics G = Graphics.FromImage(filter))
+            {
+                G.DrawImage(control.Image, new Rectangle(0, 0, filter.Width, filter.Height), 0, 0, filter.Width, filter.Height, GraphicsUnit.Pixel, IA);
+            }
+            control.Image = filter;
+            this.Text = "Color - Ready !";
+            this.Cursor = System.Windows.Forms.Cursors.Arrow;
+        }
+
+        void DragOn()
+        {
+            
+            drag = true;
+            x = System.Windows.Forms.Cursor.Position.X - this.Left;
+            y = System.Windows.Forms.Cursor.Position.Y - this.Top;
+        }
+
+        void DragOff()
+        {
+            drag = false;
+        }
+
+        void Displace() {
+            if (drag)
+            {
+                this.Top = System.Windows.Forms.Cursor.Position.Y - y;
+                this.Left = System.Windows.Forms.Cursor.Position.X - x;
+            }
+        }
+
+        private void Main_MouseDown(object sender, MouseEventArgs e)
+        {
+            DragOn();
+        }
+
+        private void Main_MouseUp(object sender, MouseEventArgs e)
+        {
+            DragOff();
+        }
+
+        private void Main_MouseMove(object sender, MouseEventArgs e)
+        {
+            Displace();
+        }
+
+        private void ResetFrame_Click(object sender, EventArgs e)
+        {
+            MainFrame.Image = source;
+        }
+
+        private void ContrastFrame_Click(object sender, EventArgs e)
+        {
+            MainFrame.Image = ContrastFrame.Image;
+        }
+
+        private void BNFrame_Click(object sender, EventArgs e)
+        {
+            MainFrame.Image = BNFrame.Image;
+        }
+
+        private void BlueFrame_Click(object sender, EventArgs e)
+        {
+            MainFrame.Image = BlueFrame.Image;
+        }
+
+        private void RedFrame_Click(object sender, EventArgs e)
+        {
+            MainFrame.Image = RedFrame.Image;
+        }
+
+        private void SepiaFrame_Click(object sender, EventArgs e)
+        {
+            MainFrame.Image = SepiaFrame.Image;
+        }
+
+        private void UltraRedFrame_Click(object sender, EventArgs e)
+        {
+            MainFrame.Image = UltraRedFrame.Image;
+        }
+
+        private void PinkFrame_Click(object sender, EventArgs e)
+        {
+            MainFrame.Image = PinkFrame.Image;
+        }
+
+        private void VintageFrame_Click(object sender, EventArgs e)
+        {
+            MainFrame.Image = VintageFrame.Image;
+        }
+
+        private void MainFrame_Click(object sender, EventArgs e)
+        {
+            saveFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+            saveFileDialog.Filter = "Bitmap Image (.bmp)|*.bmp|JPEG Image (.jpeg)|*.jpeg|Png Image (.png)|*.png";
+            saveFileDialog.ShowDialog();
+        }
+
+        private void saveFileDialog_FileOk(object sender, CancelEventArgs e)
+        {           
+            MainFrame.Image.Save(saveFileDialog.FileName);
+        }
+
+        private void MainFrame_MouseMove(object sender, MouseEventArgs e)
+        {
+            this.Cursor = System.Windows.Forms.Cursors.Hand;
+        }
+
+        private void MainFrame_MouseLeave(object sender, EventArgs e)
+        {
+            this.Cursor = System.Windows.Forms.Cursors.Arrow;
+        }
+
+        private void ResetFrame_MouseMove(object sender, MouseEventArgs e)
+        {
+            this.Cursor = System.Windows.Forms.Cursors.Hand;
+        }
+
+        private void ResetFrame_MouseLeave(object sender, EventArgs e)
+        {
+            this.Cursor = System.Windows.Forms.Cursors.Arrow;
+        }
+
+        private void ContrastFrame_MouseMove(object sender, MouseEventArgs e)
+        {
+            this.Cursor = System.Windows.Forms.Cursors.Hand;
+        }
+
+        private void ContrastFrame_MouseLeave(object sender, EventArgs e)
+        {
+            this.Cursor = System.Windows.Forms.Cursors.Arrow;
+        }
+
+        private void BNFrame_MouseMove(object sender, MouseEventArgs e)
+        {
+            this.Cursor = System.Windows.Forms.Cursors.Hand;
+        }
+
+        private void BNFrame_MouseLeave(object sender, EventArgs e)
+        {
+            this.Cursor = System.Windows.Forms.Cursors.Arrow;
+        }
+
+        private void BlueFrame_MouseMove(object sender, MouseEventArgs e)
+        {
+            this.Cursor = System.Windows.Forms.Cursors.Hand;
+        }
+
+        private void BlueFrame_MouseLeave(object sender, EventArgs e)
+        {
+            this.Cursor = System.Windows.Forms.Cursors.Arrow;
+        }
+
+        private void RedFrame_MouseMove(object sender, MouseEventArgs e)
+        {
+            this.Cursor = System.Windows.Forms.Cursors.Hand;
+        }
+
+        private void RedFrame_MouseLeave(object sender, EventArgs e)
+        {
+            this.Cursor = System.Windows.Forms.Cursors.Arrow;
+        }
+
+        private void SepiaFrame_MouseMove(object sender, MouseEventArgs e)
+        {
+            this.Cursor = System.Windows.Forms.Cursors.Hand;
+        }
+
+        private void SepiaFrame_MouseLeave(object sender, EventArgs e)
+        {
+            this.Cursor = System.Windows.Forms.Cursors.Arrow;
+        }
+
+        private void UltraRedFrame_MouseMove(object sender, MouseEventArgs e)
+        {
+            this.Cursor = System.Windows.Forms.Cursors.Hand;
+        }
+
+        private void UltraRedFrame_MouseLeave(object sender, EventArgs e)
+        {
+            this.Cursor = System.Windows.Forms.Cursors.Arrow;
+        }
+
+        private void PinkFrame_MouseMove(object sender, MouseEventArgs e)
+        {
+            this.Cursor = System.Windows.Forms.Cursors.Hand;
+        }
+
+        private void PinkFrame_MouseLeave(object sender, EventArgs e)
+        {
+            this.Cursor = System.Windows.Forms.Cursors.Arrow;
+        }
+
+        private void VintageFrame_MouseMove(object sender, MouseEventArgs e)
+        {
+            this.Cursor = System.Windows.Forms.Cursors.Hand;
+        }
+
+        private void VintageFrame_MouseLeave(object sender, EventArgs e)
+        {
+            this.Cursor = System.Windows.Forms.Cursors.Arrow;
+        }
+    }
+}
